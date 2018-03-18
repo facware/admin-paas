cff63afdc11c8f84d0d57bc018d29bb9779a60a0
Luis Espinoza <luis.espinoza@buildbinder.com>
2018-03-18 14:35:36 -0700

    added data table js css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316004915) do
+ActiveRecord::Schema.define(version: 20180318195315) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
