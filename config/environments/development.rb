c7d21f9c06cf7f8afd0d7663caadf71ee6dc7f1c
Luis Espinoza <luis.espinoza@buildbinder.com>
2018-03-20 20:51:24 -0700

    added migration to devise account to ask confirmation and configured smtp client

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
+  
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.zoho.com",
+    :port => 587,
+    :user_name => ENV["FACWARE_CONTACT_MAIL"],
+    :password => ENV["FACWARE_CONTACT_PASSWORD"],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+ }
 end
